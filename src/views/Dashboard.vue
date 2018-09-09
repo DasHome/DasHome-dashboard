092710a310145efdff5d3a31b020a50a9ccc0efe
Ismaïl <nguyen.ismail@gmail.com>
2018-09-09 17:53:07 +0200

    Add Dashboard view

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container-fluid">
+        <Header />
+        <Panel />
+    </div>
+</template>
+
+<script>
+    import Header from '../components/Header.vue'
+    import Panel from '../components/Panel.vue'
+
+    export default {
+        components: {
+            Header,
+            Panel
+        }
+    }
+</script>
