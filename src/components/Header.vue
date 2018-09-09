a9569e2f1ea42ea3a81f665907bc63e8cb7d3cdf
Ismaïl <nguyen.ismail@gmail.com>
2018-09-09 17:53:28 +0200

    Add dashboard header component

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row">
+        <div class="col-md-12">
+            <h1><strong>{{ welcomeMessage }}</strong></h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                welcomeMessage: ''
+            }
+        },
+        methods: {
+            fetchDatas () {
+                this.welcomeMessage = 'Good morning !'
+            }
+        },
+        mounted() {
+            this.fetchDatas();
+        }
+    }
+</script>
